f4b957879e6f705667a80fedb080e0d14b4a4308
thomas-stanley <111903534+thomas-stanley@users.noreply.github.com>
2025-02-26 20:36:05 +0000

    Created the parser that processes the hdl file and splits it up into a dictionary containing the inputs, outputs, and body of each module

---
+++ hdl_simulator/parser.py
@@ -0,0 +1,36 @@
+import re
+
+class HDLParser:
+    def __init__(self, filename):
+        # Initialise with a HDL file path
+        self.filename = filename
+        self.modules = {}  # Dictionary for parsed modules
+
+    def parse(self):  # Need to add functionality for comments within the hdl file and for hdl files being able to include other hdl files
+        # Reads HDL file and parses it by extracting module definitions
+        with open(self.filename, "r") as file:
+            content = file.read()
+        module_pattern = re.findall(r"MODULE (\w+)\((.*?) -> (.*?)\)(.*?)END", content, re.DOTALL)  # \( and \) match literal parentheses
+        for module in module_pattern:
+            name, inputs, outputs, body = module
+
+            input_list = [module_input.strip() for module_input in inputs.split(",")]  # Separates the inputs
+            output_list = [module_output.strip() for module_output in outputs.split(",")]  # Separates the outputs
+
+            body_statements = re.findall(r"(\w+)\s*=\s*(.+);", body)  # Extracts the body statements and separates them into a list of tuples
+
+            self.modules[name] = {
+                "inputs": input_list,
+                "outputs": output_list,
+                "body": body_statements
+            }
+            # print(body_statements) 
+        return self.modules
+
+def main():
+    parser = HDLParser("examples/gates.hdl")
+    modules = parser.parse()
+    print(modules)
+
+if __name__ == "__main__":
+    main()
