c9b9766fa656bb9afbc4f9ff12755b05a979e8b4
thomas-stanley <111903534+thomas-stanley@users.noreply.github.com>
2025-03-02 19:01:00 +0000

    Updated to now work with the way I've rewritten the parser code and also to work with the module classes

--- hdl_simulator/simulator.py
+++ hdl_simulator/simulator.py
@@ -1,10 +1,9 @@
-from parser import HDLParser
+from parser import parse_hdl
 
 class CircuitSimulator:
     def __init__(self, filename):
         # Parses the HDL file and initialises the simulator with the parsed modules
-        self.parser = HDLParser(filename)
-        self.modules = self.parser.parse()  # Parsed modules stored here
+        self.modules = {module.name: module for module in parse_hdl(filename)}  # Parsed modules stored here
 
     def evaluate(self, module_name, inputs):
         # Evaluates a module given its name and inputs
@@ -13,7 +12,6 @@ class CircuitSimulator:
                 raise ValueError(f"Expected 2 inputs for {module_name} module, got {len(inputs)}.")
 
             return {"out": int(not(inputs["a"] and inputs["b"]))}
-        
         if module_name not in self.modules:
             raise ValueError(f"Module {module_name} not found.")
         
@@ -24,9 +22,9 @@ class CircuitSimulator:
             inputs = {"a": tuple(inputs.values())[0]}
 
         module = self.modules[module_name]
-        input_variables = module["inputs"]
+        input_variables = module.inputs
         # output_variables = module["outputs"] This will need to be sorted out when there are multiple outputs
-        logic = module["body"]
+        logic = module.body
         variables = inputs.copy()
 
         if len(input_variables) != len(inputs):
@@ -34,9 +32,8 @@ class CircuitSimulator:
 
         for line in logic:
             variable, expression = line
-            function = expression.split("(")[0]
-            arguments = expression.split("(")[1].split(")")[0].split(",")
-            arguments = [arg.strip() for arg in arguments]
+            function = expression["module"]
+            arguments = expression["args"]
 
             if "Nand" == function:
                 variables[variable] = int(not (variables[arguments[0]] and variables[arguments[1]]))
@@ -51,7 +48,7 @@ def main():
     simulator = CircuitSimulator("examples/gates.hdl")
     inputs = {"a": 1, "b": 0}
     # inputs = {"a": 0}
-    outputs = simulator.evaluate("And", inputs)
+    outputs = simulator.evaluate("And", inputs)  # Need to write some tests for this to speed up debugging
     print(outputs)
 
 if __name__ == "__main__":
