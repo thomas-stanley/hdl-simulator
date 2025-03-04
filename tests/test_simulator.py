e64a24915dcddd030765be6e309f737df80a9b6d
Thomas Stanley <111903534+thomas-stanley@users.noreply.github.com>
2025-03-04 11:54:27 +0000

    Removed unused import

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -4,7 +4,7 @@ import pytest
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from hdl_simulator import CircuitSimulator, parse_hdl
+from hdl_simulator import CircuitSimulator
 
 @pytest.fixture(scope="module")
 def simulator():  # So I only have to create an instance once
