43dabe46960b59d5d6403ce78c9982334cbda3a8
thomas-stanley <111903534+thomas-stanley@users.noreply.github.com>
2025-03-19 21:42:46 +0000

    Added more tests for erroneous inputs

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -56,3 +56,37 @@ def test_not(simulator, a, expected):
 def test_xor(simulator, a, b, expected):
     result = simulator.evaluate("Xor", {"a": a, "b": b})
     assert result["out"] == expected
+
+
+@pytest.mark.parametrize("inputs", 
+                         [{},  # Missing both inputs
+                          {"a": 0},  # Missing one input
+                          {"a": 0, "b": 0, "c": 0}])  # Extra input
+def test_nand_input_validation(simulator, inputs):
+    with pytest.raises(ValueError):
+        simulator.evaluate("Nand", inputs)
+
+
+@pytest.mark.parametrize("inputs",
+                         [{},  # Missing input
+                          {"a": 0, "b": 0}])  # Extra input
+def test_not_input_validation(simulator, inputs):
+    with pytest.raises(ValueError):
+        simulator.evaluate("Not", inputs)
+
+
+@pytest.mark.parametrize("module, inputs, error_type",
+                         [("And", {"a": "string_1", "b": "string_2"}, TypeError),
+                          ("Or", {"a": 1.5, "b": 0.5}, TypeError),
+                          ("Xor", {"a": -1, "b": -2}, ValueError),
+                          ("And", {"a": 2, "b": 1}, ValueError)])
+def test_erroneous_input(simulator, module, inputs, error_type):
+    with pytest.raises(error_type):
+        simulator.evaluate(module, inputs)
+
+
+def test_unknown_module(simulator):
+    with pytest.raises(ValueError, match="Module 'InvalidModule' not found."):
+        simulator.evaluate("InvalidModule", {"a": 0, "b": 0})
+
+
